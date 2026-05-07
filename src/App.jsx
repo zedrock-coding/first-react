ae94a0028e7addbf4635ea3af699dc27235b2d15
Lakshya Jadhav <lakshyajadhav15@gmail.com>
2026-05-07 19:55:33 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import HeroSection from './components/hero-section.jsx';
+import DynamicDiv from './components/dynamic-div.jsx';
 import './App.css'
 
 function App() {
@@ -9,9 +10,7 @@ function App() {
     <div className = 'grid-cols-1 gap-4'>
       <HeroSection />
       <section className='flex flex-no-wrap align-item-center justify-content-center bg-gray-100 dark:bg-gray-800 py-6 mt-8 rounded-lg shadow-md'>
-        <canvas className='max-w-4xl min-h-128 px-4 py-8'>
-
-        </canvas>
+        <DynamicDiv />
       </section>
     </div>
   )
--- src/components/dynamic-div.jsx
+++ src/components/dynamic-div.jsx
@@ -2,8 +2,14 @@ import useMousePosition from './mouse_logic.jsx';
 import React from 'react';
 import '../App.css'
 
+const canvas = <canvas className='max-w-4xl min-h-128 px-4 py-8' id='myCanvas'> {DynamicDiv}</canvas>;
 function DynamicDiv() {
-    return <div>Dynamic Div</div>;
+    const dynamicDiv = <div id='move_resize_div' className = 'flex flex-no-wrap align-item-center justify-content-center bg-gray-200 dark:bg-gray-500 py-6 mt-8 rounded-lg shadow-md h-32 w-32'></div>
+    return canvas;
 }
-
+function mouseDown(e){
+    e.addEventListener('mousedown', (e) =>{
+        let posX = updatePostion.x - e.clientX;
+    })
+}
 export default DynamicDiv;
